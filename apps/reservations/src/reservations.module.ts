09148e4faf2d7d080f83860f7c92f921daefb14a
Niyaz Jamal <niyaz.jamal@carexpert.com.au>
2024-11-24 12:07:40 +0800

    add logger service with pino and validations

--- apps/reservations/src/reservations.module.ts
+++ apps/reservations/src/reservations.module.ts
@@ -7,6 +7,7 @@ import {
   ReservationSchema,
 } from './models/reservation.schema';
 import { ReservationRepository } from './reservations.repository';
+import { LoggerModule } from '@app/common';
 
 @Module({
   imports: [
@@ -14,6 +15,7 @@ import { ReservationRepository } from './reservations.repository';
     DatabaseModule.forFeature([
       { name: ReservationDocument.name, schema: ReservationSchema },
     ]),
+    LoggerModule,
   ],
   controllers: [ReservationsController],
   providers: [ReservationsService, ReservationRepository],
