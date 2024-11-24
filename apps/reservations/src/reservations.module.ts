053f72be8328a678108587d4526b458013d880ff
Niyaz Jamal <niyaz.jamal@carexpert.com.au>
2024-11-24 18:06:19 +0800

    Add Jwt and refactor config service to each services

--- apps/reservations/src/reservations.module.ts
+++ apps/reservations/src/reservations.module.ts
@@ -1,4 +1,7 @@
 import { Module } from '@nestjs/common';
+import { LoggerModule } from '@app/common';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 import { ReservationsService } from './reservations.service';
 import { ReservationsController } from './reservations.controller';
 import { DatabaseModule } from '@app/common/database';
@@ -7,7 +10,6 @@ import {
   ReservationSchema,
 } from './models/reservation.schema';
 import { ReservationRepository } from './reservations.repository';
-import { LoggerModule } from '@app/common';
 
 @Module({
   imports: [
@@ -16,6 +18,13 @@ import { LoggerModule } from '@app/common';
       { name: ReservationDocument.name, schema: ReservationSchema },
     ]),
     LoggerModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        PORT: Joi.number().required(),
+        MONGODB_URI: Joi.string().required(),
+      }),
+    }),
   ],
   controllers: [ReservationsController],
   providers: [ReservationsService, ReservationRepository],
